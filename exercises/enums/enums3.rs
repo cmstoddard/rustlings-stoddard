5b6f7cf670495f9bd08b60a4bcdcd67f4480c067
cmstoddard <christopher@stoddard.dev>
2022-09-22 00:04:30 -0500

    finished enums3.rs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,15 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
-    ChangeColor((u8,u8,u8)),
+    ChangeColor((u8, u8, u8)),
     Echo(String),
     Move(Point),
     Quit,
-
 }
 
 struct Point {
@@ -43,9 +40,14 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(value) => self.color = value,
+            Message::Echo(value) => println!("{}", value),
+            Message::Move(value) => self.position = value,
+            Message::Quit => self.quit = true,
+        }
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
