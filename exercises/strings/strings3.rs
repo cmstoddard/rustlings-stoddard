1a945775442dfbbf6a26da4f2462d27b4e769013
cmstoddard <christopher@stoddard.dev>
2022-09-22 16:38:15 -0500

    finished strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,26 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+
+    let mut mut_input = input.trim();
+    return String::from(mut_input);
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    //input = input + " world!";
+    let mut o_string = input.to_owned();
+    let world: &str = " world!";
+    o_string.push_str(world);
+    return String::from(o_string);
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let m_input = &input.replace("cars", "balloons");
+    return String::from(m_input);
 }
 
 #[cfg(test)]
@@ -37,7 +42,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
